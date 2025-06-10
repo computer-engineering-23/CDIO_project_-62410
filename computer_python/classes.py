2b3a67fbb1672c7c1b705003c870eff6bc3ac449
dendersen <dendersen@endersen.dk>
2025-06-10 15:50:39 +0200

    beginning pathfinder

--- computer_python/classes.py
+++ computer_python/classes.py
@@ -1,11 +1,17 @@
 import math
 import numpy as np
+from typing import List
+
+class Point:
+    pass
 
 class Point:
     """Point class to represent a point in 2D space"""
     def __init__(self, x:int, y:int):
         self.x = x
         self.y = y
+    def __eq__(self, other: Point):
+        return self.x == other.x and self.y == other.y
 
 class Movement:
     """Movement class to represent a movement in 2D space"""
@@ -14,7 +20,10 @@ class Movement:
         self.direction = direction
 
 class Rotation:
-    """Rotation class to represent an angle change in radians"""
+    """
+        Rotation class to represent an angle change in radians
+        possitive angle is counter-clockwise, negative angle is clockwise
+    """
     def __init__(self, angle:float):
         self.angle = angle
     def __init__(self, PointBase0:Point,PointBase1:Point, PointTarget:Point):
@@ -24,4 +33,25 @@ class Wall:
     def __init__(self, wall:np.array):
         self.wall = wall
         self.start = Point(wall[0][1], wall[0][0])
-        self.end = Point(wall[1][1], wall[1][0])
+        self.end = Point(wall[0][3], wall[0][2])
+
+class Car:
+    def __init__(self, car:List[np.array], front:List[np.array] = None):
+        self.triangle = []
+        for point in car:
+            self.triangle.append(Point(point[0][1], point[0][0]))
+        self.front = Point(front[0][1], front[0][0]) if front is not None and len(front) == 2  else Point(0, 0)
+        
+        while(len(self.triangle) < 3):
+            self.triangle.append(Point(0, 0))
+        if(len(self.triangle) > 3):
+            self.triangle = self.triangle[:3]
+        
+        while(not self.triangle[0] == self.front):
+            self.triangle.append(self.triangle.pop(0))
+        self.base = self.triangle[1:3]
+
+class Pickup:
+    """Pickup class to represent a pickup action"""
+    def __init__(self):
+        pass
--- computer_python/path_find.py
+++ computer_python/path_find.py
@@ -1,39 +1,38 @@
 import cv2
 import numpy as np
 from typing import List, Tuple, Union
-from computer_python.image_recognition import Camera
-from computer_python.classes import Point, Movement, Rotation, Wall
-
+from image_recognition import Camera
+from classes import Point, Movement, Rotation, Wall, Car, Pickup
+import math
 
 class track:
     def __init__(self,cam:cv2.VideoCapture,walls:List[np.array] = None, goals:List[Tuple[int,int]] = None, targets:List[Tuple[int,int]] = None, obsticles: List[Tuple[int,int]] = None, car:List[Tuple[int,int]] = None, front:Tuple[int,int] = None):
-        self.walls:List[np.array] = walls
-        self.goals:List[Tuple[int,int]] = goals
-        self.targets:List[Tuple[int,int]] = targets
+        self.walls:List[Wall] = self.formatWalls(walls)
+        self.goals:List[Point] = self.formatGoals(goals)
+        self.targets:List[Point] = self.formatTargets(targets)
         self.cam:cv2.VideoCapture = cam
         if(cam is None):
             self.cam = Camera()
-        self.obsticles = obsticles
-        self.car:List[Tuple[int,int]] = car
-        self.front:Tuple[int,int] = front
+        self.obsticles = self.formatObsticles(obsticles)
+        self.car:Car = self.formatCar(car, [front])
     
     def update(self, walls:bool = False, goals:bool = False, targets:bool = False, obsticles:bool = False, car:bool = False):
         frame:np.array = self.cam.getFrame()
         
         if(walls):
-            self.walls = self.cam.generateWall(40)
+            self.walls = self.formatWalls(self.cam.generateWall(40))
         
         if(goals):
-            self.goals = self.cam.midpointWalls(self.cam.shape[1], self.walls)
+            self.goals = self.formatGoals(self.cam.midpointWalls(self.cam.shape[1], self.cam.walls))
         
         if(targets):
-            self.targets = self.cam.findCircle(np.copy(frame))
+            self.targets = self.formatTargets(self.cam.findCircle(np.copy(frame)))
         
         if(obsticles is not None):
-            self.obsticles = self.cam.findEgg(np.copy(frame))
+            self.obsticles = self.formatObsticles(self.cam.findEgg(np.copy(frame)))
         
         if(car):
-            self.car, self.front = self.cam.findCar(np.copy(frame))
+            self.car = self.formatCar(*self.cam.findCar(np.copy(frame)))
     
     def formatWalls(self, walls:List[np.array]) -> List[Wall]:
         realWalls = []
@@ -42,11 +41,36 @@ class track:
         for wall in walls:
             realWalls.append(Wall(wall))
         return realWalls
+    def formatGoals(self, goals:List[Tuple[int,int]]) -> List[Point]:
+        realGoals = []
+        if(goals is None or len(goals) == 0):
+            return []
+        for goal in goals:
+            realGoals.append(Point(goal[1], goal[0]))
+        return realGoals
+    def formatTargets(self, targets:List[Tuple[int,int]]) -> List[Point]:
+        realTargets = []
+        if(targets is None or len(targets) == 0):
+            return []
+        for target in targets:
+            realTargets.append(Point(target[0][1], target[0][0]))
+        return realTargets
+    def formatObsticles(self, obsticles:List[Tuple[int,int]]) -> List[Point]:
+        realObsticles = []
+        if(obsticles is None or len(obsticles) == 0):
+            return []
+        for obsticle in obsticles:
+            realObsticles.append(Point(obsticle[0][1], obsticle[0][0]))
+        return realObsticles
+    def formatCar(self, car:List[Tuple[int,int]], front:Tuple[int,int]) -> Car:
+        if(car is None or len(car) == 0):
+            return Car([[(0, 0)],[(0, 1)],[(0, 2)]], [(0, 0)])
+        return Car(car, front)
     
     def VectorPath(self) -> Tuple[int,int]:
         """generates Vector from front to first goal"""
         target:Tuple[int,int] = self.goals[0]
-        return (self.front[0] - target[0], self.front[1] - target[1])
+        return Point(self.front[0] - target[0], self.front[1] - target[1])
     
     def generatepath(self) -> List[Union[Rotation,Movement]]:
         """Generates a path from the car to the first goal"""
@@ -54,6 +78,54 @@ class track:
             return []
         path = []
         vector = self.VectorPath()
+        if(vector.x == 0 and vector.y == 0):
+            return [Pickup()]
         
+        if(vector.y > 0):
+            path.append(Rotation(math.pi / 2 - self.car.getRotation()))
+            path.append(Movement(vector.y,math.pi / 2))
+        elif(vector.y < 0):
+            path.append(Rotation(-math.pi / 2 - self.car.getRotation()))
+            path.append(Movement(vector.y,-math.pi / 2))
         
-        return path
+        if(vector.x > 0):
+            path.append(Rotation(0 - path[-1].direction))
+            path.append(Movement(vector.x, 0))
+        elif(vector.x < 0):
+            path.append(Rotation(math.pi - path[-1].direction))
+            path.append(Movement(vector.x, math.pi))
+
+        return path + [Pickup()]
+    
+    def Draw(self, frame:np.array):
+        """Draws the track on the frame"""
+        for wall in self.walls:
+            cv2.line(frame, (int(wall.start.x), int(wall.start.y)), (int(wall.end.x), int(wall.end.y)), (0, 0, 255), 1)
+        
+        for goal in self.goals:
+            cv2.circle(frame, (int(goal.x), int(goal.y)), 5, (255, 0, 0), -1)
+        
+        for target in self.targets:
+            cv2.circle(frame, (int(target.x), int(target.y)), 5, (0, 255, 0), -1)
+        
+        for obsticle in self.obsticles:
+            cv2.circle(frame, (int(obsticle.x), int(obsticle.y)), 5, (0, 255, 255), -1)
+        
+        for i in range(len(self.car.triangle)):
+            p0 = self.car.triangle[i]
+            p1 = self.car.triangle[(i + 1) % 3]
+            cv2.line(frame, (int(p0.y), int(p0.x)), (int(p1.y), int(p1.x)), (255, 255, 0), 2)
+        
+        return frame
+
+    def test(self):
+        """Test function to show the track"""
+        while True:
+            self.update(walls=False, goals=False, targets=True, obsticles=True, car=True)
+            frame = self.cam.getFrame()
+            self.Draw(frame)
+            self.cam.displayFrame(frame,"Track")
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+        self.cam.close()
+        cv2.destroyAllWindows()
